013b7214e8634e14d90f2121ef117ba6493beb47
Brandon Skaggs <bls8948@gmail.com>
2017-07-03 18:24:28 -0400

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,7 @@
+var beReady =  confirm("Do you want to see awesome webpage examples?")
+while (beReady == false) 
+var beReady = confirm("Do you want to see awesome webpage examples?")
+if (beReady = true)
+alert('Thanks for checking out my site(:');
+
+
---
+++ future.js
@@ -0,0 +1,17 @@
+var beready = confirm('Are you a futurama whizz?');
+while(beready == false) {
+var beready = confirm('Are you fucking ready or not!?')
+}
+if(var beready == true) {
+var question1 = prompt('Which character has one eye?');
+var question2 = prompt('Which character has 3 eyes?');
+var question3 = prompt('What is fry\'s favorite drink?');
+var question4 = prompt('What do robots drink to survive?');
+var question5 = prompt('Who is the professors uncle?');
+}
+if(question1.toLowerCase() == 'leela' && question2.toLowerCase() == 'nibbler' && question3.toLowerCase() == 'slurm' && question4.toLowerCase() == 'beer' && question5.toLowerCase() == 'fry' ) {
+alert('you passed 100%');
+} else if(question1 != 'leela' || question2 != 'nibbler' || question3 != 'slurm' || question4 != ' beer' || question5 != fry) {
+alert('You fucking suck. ' '\nGo watch more tv bro.')
+}
+
